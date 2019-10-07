34004d9fd3818411935844152d30de7caa92e1e1
Andy Liang <meteochu@gmail.com>
2019-10-06 22:39:58 -0400

    Add basic multi-window support on iOS 13 with scenes

--- Blurry/AppDelegate.swift
+++ Blurry/AppDelegate.swift
@@ -9,19 +9,35 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate : UIResponder, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        if #available(iOS 13.0, *) {
+            // no customization in iOS 13+
+        } else {
+            let attributes: [NSAttributedString.Key: Any] = [
+                .font: UIFont.boldSystemFont(ofSize: 12),
+                .kern: 2.0
+            ]
+            UISegmentedControl.appearance().setTitleTextAttributes(attributes, for: .normal)
+        }
+        return true
+    }
+}
 
-  var window: UIWindow?
-
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-	let attributes: [NSAttributedString.Key: Any] = [
-		.font: UIFont.systemFont(ofSize: 12, weight: .bold),
-		.kern: 2.0
-	]
-    UISegmentedControl.appearance().setTitleTextAttributes(attributes, for: .normal)
-    
-    return true
-  }
-
+@available(iOS 13.0, *) // iOS 13 Scene Support
+extension AppDelegate {
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        let config = UISceneConfiguration(
+            name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        config.delegateClass = SceneDelegate.self
+        config.storyboard = UIStoryboard(name: "Main", bundle: .main)
+        return config
+    }
 }
---
+++ Blurry/SceneDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  SceneDelegate.swift
+//  Blurry
+//
+//  Created by Andy Liang on 2019-10-06.
+//  Copyright © 2019 Andy Liang. All rights reserved.
+//
+
+import UIKit
+
+@available(iOS 13.0, *)
+class SceneDelegate : UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+}
